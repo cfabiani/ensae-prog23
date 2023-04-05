1b6518b636b4e2c5e039b4645090f9f703b9d7ff
cfabiani <cesar.fabiani@sciencespo.fr>
2023-04-05 12:45:29 +0000

    hh

--- delivery_network/Brouillon.py
+++ delivery_network/Brouillon.py
@@ -20,7 +20,7 @@ dot.render('exemple_graphique', view=True)
 """
 
 from graph import Graph, graph_from_file
-
+"""
 g = Graph([k for k in range(5)])
 
 g.add_edge(0, 1, 10)
@@ -35,7 +35,7 @@ g2 = g.kruskal()
 
 
 a , b = g2.find_parents(1)
-
+"""
 #print(g2.get_path_opti(3, 6, a, b))
 
 #print(g)
@@ -63,13 +63,111 @@ profondeurs, parents = h.find_parents(1)
 print(h.get_path_opti(4, 5, profondeurs, parents))
 """
 
-
+"""
 h = graph_from_file("input/network.00.in")
 h_mst = h.kruskal()
-
+"""
 
 
 #print(h.get_path_with_power(1, 9, 50))
 #print(h.min_power(1, 5))
 #print(h.min_power_mst(1, 5))
 
+def camion_par_routes2(dict_route, dict_camion, tree):
+
+    """
+    On cherche à améliorer le dicitionnaire des routes.
+    Pour l'instant on a pour chaque trajet (numéroté par un entier),
+    une liste avec [source, destinationation, utilité].
+    On cherche pour chacun de ces trajets, à rajouter le min_power
+    pour effectuer le trajet et le meilleur camion.
+    Le meilleur camion est le camion le moins cher
+    parmi tous les camions qui ont la puissance nécéssaire.
+    Comme on a un stock infini de camions, on a pas à
+    s'inquiéter si un camion est mis pour plusieurs trajets.
+
+    Paramètres:
+        -----------
+        dict_route: dict
+            Dictionnaire des trajets qui à chaque trajet (numéroté par un
+            entier) associe la liste [source, destination, gain]
+        dict_camion: dict
+            Dictionnaire des camions qui à chaque camion (numéroté par un
+            entier) associe la liste [puissance, cout]
+        tree : Graph
+            Arbre couvrant minimal associé aux routes et camions
+
+        Résultats :
+        -----------
+        XX
+    """
+
+    dict_route_complete = dict_route
+
+    profondeurs, parents = tree.find_parents(1)
+
+    check = 0
+
+    for trajet in dict_route_complete:
+        check = check + 1
+        print(check)
+        src, dest, gain = dict_route[trajet]
+        # On cherche le min_power du trajet
+        inutile, p = tree.min_power_opti(src, dest, profondeurs, parents)
+        dict_route_complete[trajet].append(p)
+        # On trouve les camions qui peuvent effectuer le trajet
+        #camions_possibles = []
+        
+        """
+        for camion in dict_camion:
+            indice_camion = indice_camion + 1
+            puissance = dict_camion[camion][0]
+            #cout = dict_camion[camion][1]
+            if puissance >= p:
+                a = dict_camion[camion].copy()
+                #a.append(indice_camion)
+                camions_possibles.append(a)
+                print(camions_possibles)
+        # On trouve celui au coût minimal
+        cout_minimal = min([i[1] for i in camions_possibles])
+        indice = [i[1] for i in camions_possibles].index(cout_minimal)
+        meilleur_camion = camions_possibles[indice]
+        dict_route_complete[trajet].append(meilleur_camion[1])
+        """
+        indice_camion = 0
+        meilleur_camion = 0
+        coutmin = dict_camion[1][1]
+
+        for camion in dict_camion:
+            indice_camion += 1
+            puissance = dict_camion[camion][0]
+            if puissance >= p:
+                if dict_camion[camion][1] <= coutmin:
+                    coutmin = dict_camion[camion][1]
+                    meilleur_camion = indice_camion
+                    print(meilleur_camion)
+
+        dict_route_complete[trajet].append(meilleur_camion)
+                
+    return dict_route_complete
+
+
+def knapsack2(B, pt, ut, n):
+   
+    """_summary_
+
+    Args:
+        B (int): Budget
+        pt (prix): _description_
+        ut (_type_): _description_
+        n (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if n==0 or B==0:
+        return 0
+    if pt[n-1]>B:
+        return knapsack(B, pt, ut, n-1)
+    else:
+        return max(ut[n-1]+knapsack(B-pt[n-1],pt,ut,n-1),knapsack(B, pt, ut, n-1))
--- delivery_network/graph.py
+++ delivery_network/graph.py
@@ -743,103 +743,6 @@ def routes_from_file(filename):
     return R
 
 
-def camion_par_routes2(dict_route, dict_camion, tree):
-
-    """
-    On cherche à améliorer le dicitionnaire des routes.
-    Pour l'instant on a pour chaque trajet (numéroté par un entier),
-    une liste avec [source, destinationation, utilité].
-    On cherche pour chacun de ces trajets, à rajouter le min_power
-    pour effectuer le trajet et le meilleur camion.
-    Le meilleur camion est le camion le moins cher
-    parmi tous les camions qui ont la puissance nécéssaire.
-    Comme on a un stock infini de camions, on a pas à
-    s'inquiéter si un camion est mis pour plusieurs trajets.
-
-    Paramètres:
-        -----------
-        dict_route: dict
-            Dictionnaire des trajets qui à chaque trajet (numéroté par un
-            entier) associe la liste [source, destination, gain]
-        dict_camion: dict
-            Dictionnaire des camions qui à chaque camion (numéroté par un
-            entier) associe la liste [puissance, cout]
-        tree : Graph
-            Arbre couvrant minimal associé aux routes et camions
-
-        Résultats :
-        -----------
-        XX
-    """
-
-    dict_route_complete = dict_route
-
-    profondeurs, parents = tree.find_parents(1)
-
-    check = 0
-
-    for trajet in dict_route_complete:
-        check = check + 1
-        print(check)
-        src, dest, gain = dict_route[trajet]
-        # On cherche le min_power du trajet
-        inutile, p = tree.min_power_opti(src, dest, profondeurs, parents)
-        dict_route_complete[trajet].append(p)
-        # On trouve les camions qui peuvent effectuer le trajet
-        #camions_possibles = []
-        """
-        for camion in dict_camion:
-            indice_camion = indice_camion + 1
-            puissance = dict_camion[camion][0]
-            #cout = dict_camion[camion][1]
-            if puissance >= p:
-                a = dict_camion[camion].copy()
-                #a.append(indice_camion)
-                camions_possibles.append(a)
-                print(camions_possibles)
-        # On trouve celui au coût minimal
-        cout_minimal = min([i[1] for i in camions_possibles])
-        indice = [i[1] for i in camions_possibles].index(cout_minimal)
-        meilleur_camion = camions_possibles[indice]
-        dict_route_complete[trajet].append(meilleur_camion[1])
-        """
-        indice_camion = 0
-        meilleur_camion = 0
-        coutmin = dict_camion[1][1]
-
-        for camion in dict_camion:
-            indice_camion += 1
-            puissance = dict_camion[camion][0]
-            if puissance >= p:
-                if dict_camion[camion][1] <= coutmin:
-                    coutmin = dict_camion[camion][1]
-                    meilleur_camion = indice_camion
-                    print(meilleur_camion)
-
-        dict_route_complete[trajet].append(meilleur_camion)
-                
-    return dict_route_complete
-
-
-def knapsack2(B, pt, ut, n):
-   
-    """_summary_
-
-    Args:
-        B (int): Budget
-        pt (prix): _description_
-        ut (_type_): _description_
-        n (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    if n==0 or B==0:
-        return 0
-    if pt[n-1]>B:
-        return knapsack(B, pt, ut, n-1)
-    else:
-        return max(ut[n-1]+knapsack(B-pt[n-1],pt,ut,n-1),knapsack(B, pt, ut, n-1))
 
 
 def truckperpath(dict_route, dict_camion, tree):
@@ -1022,17 +925,19 @@ def rapport(B,dict_route_complete):
 
     return Liste_camion_finale, utilite
 
+
+"""
 h = graph_from_file("input/network.2.in")
 h_mst = h.kruskal()
 r = routes_from_file("input/routes.2.in")
 t = trucks_from_file("input/trucks.2.in")
-
-z = truckperpath(r, t, h_mst)
+"""
+#z = truckperpath(r, t, h_mst)
 #print(z)
-a, b = rapport(25e9,z)
-print(b, "rapport")
-e, f, d = knapsack_greedy(25e9, z)
-print(d, "greedy")
+#a, b = rapport(25e9,z)
+#print(b, "rapport")
+#e, f, d = knapsack_greedy(25e9, z)
+#print(d, "greedy")
 #print(knapsack_dynamic(25e9, z))
 
 """
